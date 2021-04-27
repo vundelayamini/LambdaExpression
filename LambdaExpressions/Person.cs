9058ab21f92afa8c615a531b5ca2412d97a9f4df
vundelayamini <vundelayamini@gmail.com>
2021-04-27 17:08:45 +0530

    [yamini]Add: Adding the person details

---
+++ LambdaExpressions/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressions
+{
+    class Person
+    {
+        public int SSN { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LambdaExpressions
 {
@@ -7,6 +8,41 @@ namespace LambdaExpressions
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Lambda Expressions:");
+            List<Person> list = new List<Person>();
+            AddingpersonDetails(list);
+            IterateOverList(list);
+
+        }
+        //UC1-person management
+        public static void AddingpersonDetails(List<Person> personlist)
+        {
+            personlist.Add(new Person() { SSN = 1, Age = 16, Name = "yamini", Address = "Banglore" });
+            personlist.Add(new Person() { SSN = 4, Age = 22, Name = "Amani", Address = "Banglore" });
+            personlist.Add(new Person() { SSN = 5, Age = 65, Name = "Lakshmi", Address = "Hyderabad" });
+            personlist.Add(new Person() { SSN = 1, Age = 32, Name = "Priya", Address = "Manglore" });
+            personlist.Add(new Person() { SSN = 2, Age = 23, Name = "Mahi", Address = "Chennai" });
+            personlist.Add(new Person() { SSN = 7, Age = 24, Name = "Jhanu", Address = "Mumbai" });
+            personlist.Add(new Person() { SSN = 3, Age = 35, Name = "gayi", Address = "Banglore" });
+            //IterateOverList
+        }
+        public static void IterateOverList(List<Person> personlist)
+        {
+            try
+            {
+
+                foreach (Person person in personlist)
+                {
+                    Console.WriteLine("Age\t" + person.Age + "\t" + "Name\t" + person.Name + "\t" + "Address\t" + person.Address + "\t");
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+
         }
     }
+    
 }
